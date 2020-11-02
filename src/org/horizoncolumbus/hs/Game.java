eddf81a031f02117595c68b12fbfb3521be40e2a
misa <student.m.bellohernandez63@horizoncolumbus.org>
2020-11-02 13:08:40 -0500

    Add GestureList into Game.java file

--- src/org/horizoncolumbus/hs/Game.java
+++ src/org/horizoncolumbus/hs/Game.java
@@ -1,13 +1,34 @@
 package org.horizoncolumbus.hs;
 
+import java.util.ArrayList;
+
 public class Game {
 
     public boolean gameMode;
     public boolean winnerFound;
+    //Gestures
+    ArrayList<String> gestureList = new ArrayList<String>();
+    public void addGestures(){
+        gestureList.add("Spock");
+        gestureList.add("Rock");
+        gestureList.add("Scissors");
+        gestureList.add("Paper");
+        gestureList.add("Lizard");
+
+    }
+    //Print Gestures
+    public ArrayList<String> displayGestures(){
+        return gestureList;
+    }
 
     public void startGame(){
 
+
     }
+
+
+
+
 
     public void endGame(){
 
