b24d2c654f39794fb508c2cc4edc10739a55f922
Sabriyes Project <student.sbr.mohamed7@horizoncolumbus.org>
2020-11-03 09:08:52 -0500

    Rpsls 1st change

--- src/org/horizoncolumbus/hs/TwoPlayerUI.java
+++ src/org/horizoncolumbus/hs/TwoPlayerUI.java
@@ -12,6 +12,7 @@ public class TwoPlayerUI extends JFrame {
     private JButton button4;
     private JButton button5;
     private JLabel gestureLabel;
+    private JButton backButton;
     Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
     public TwoPlayerUI(){
@@ -19,6 +20,7 @@ public class TwoPlayerUI extends JFrame {
         setTitle("Two Player");
         setSize(400,290);
         setLocation(screen.width/2 - getWidth()/2, screen.height/2 - getHeight()/2);
+
 
         //Listen for window close to call back GameUI and dispose of the current
         addWindowListener(new java.awt.event.WindowAdapter() {
