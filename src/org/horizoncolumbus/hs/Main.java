a8ac99c279dcec15cd054e4ef3b5af16edb4e335
HSA-Student <student.arm.dawodi@horizoncolumbus.org>
2020-11-03 14:10:06 -0500

    Modified Main.java file to call either GUI or CLI.

--- src/org/horizoncolumbus/hs/Main.java
+++ src/org/horizoncolumbus/hs/Main.java
@@ -1,12 +1,26 @@
 package org.horizoncolumbus.hs;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-		GameUI gameUI = new GameUI();
-		gameUI.setVisible(true);
-		gameUI.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	public static void main(String[] args) {
+		System.out.print("Enter CLI for command line interface and GUI for graphical user interface: ");
+		Scanner cligui = new Scanner(System.in);
+		String userresponse = cligui.next().toLowerCase();
+		if (userresponse.equals("gui")) {
+			GameUI gameUI = new GameUI();
+			gameUI.setVisible(true);
+			gameUI.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		}else{
+			CLI gameCLI = new CLI();
+			gameCLI.welcomeMessage();
+			gameCLI.gameRules();
+			gameCLI.askForMode();
+			gameCLI.askForName("First Player");
+			gameCLI.gestures();
+			gameCLI.displayWinner("First Player");
+		}
 	}
 }
